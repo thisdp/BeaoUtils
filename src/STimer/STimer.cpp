11baeb3351a7a019da5aa30f3fb32d38247217a4
thisdp <30386052+thisdp@users.noreply.github.com>
2024-08-06 17:09:09 +0800

    First Commit

---
+++ src/STimer/STimer.cpp
@@ -0,0 +1,55 @@
+#pragma once
+#include "STimer.h"
+STimerBase::STimerBase(uint32_t tDuration) : duration(tDuration), activated(false), startTime(0){}
+bool STimerBase::start(){
+    startTime = getCurrentTime();
+    activated = true;
+    return true;
+}
+bool STimerBase::startIfNotActivated(){
+    if(activated) return true;
+    startTime = getCurrentTime();
+    activated = true;
+    return true;
+}
+bool STimerBase::restart(){
+    restartTimes++;
+    return start();
+}
+void STimerBase::clearRestartTimes(){
+    restartTimes = 0;
+}
+bool STimerBase::stop(){
+    activated = false;
+    return true;
+}
+bool STimerBase::isActivated(){
+    return activated;
+}
+uint32_t STimerBase::getStartTime(){
+    return startTime;
+}
+void STimerBase::setDuration(uint32_t tDuration){
+    duration = tDuration;
+}
+bool STimerBase::checkTimedOut(){
+    return (getCurrentTime()-startTime >= duration);
+}
+
+//STimer
+STimer::STimer(uint32_t tDuration) : STimerBase(tDuration) {}
+uint32_t STimer::getCurrentTime(){
+    return millis()/1000;
+}
+
+//MSTimer
+MSTimer::MSTimer(uint32_t tDuration) : STimerBase(tDuration) {}
+uint32_t MSTimer::getCurrentTime(){
+    return millis();
+}
+
+//USTimer
+USTimer::USTimer(uint32_t tDuration) : STimerBase(tDuration) {}
+uint32_t USTimer::getCurrentTime(){
+    return micros();
+}
---
+++ src/STimer/STimer.h
@@ -0,0 +1,38 @@
+#pragma once
+class STimerBase{
+public:
+  STimerBase(uint32_t tDuration = 0);
+  bool start();
+  bool startIfNotActivated();
+  bool restart();
+  void clearRestartTimes();
+  bool stop();
+  bool isActivated();
+  uint32_t getStartTime();
+  void setDuration(uint32_t tDuration);
+  bool checkTimedOut();
+  virtual uint32_t getCurrentTime() = 0;
+protected:
+  uint32_t duration;
+  uint32_t startTime;
+  uint32_t restartTimes;
+  bool activated;
+};
+
+class STimer : public STimerBase{
+public:
+  STimer(uint32_t tDuration = 0);
+  uint32_t getCurrentTime();
+};
+
+class MSTimer : public STimerBase{
+public:
+  MSTimer(uint32_t tDuration = 0);
+  uint32_t getCurrentTime();
+};
+
+class USTimer : public STimerBase{
+public:
+  USTimer(uint32_t tDuration = 0);
+  uint32_t getCurrentTime();
+};

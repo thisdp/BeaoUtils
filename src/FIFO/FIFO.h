387e67a2f25c1d9e5613eb9bc96f11df82691eb3
thisdp <30386052+thisdp@users.noreply.github.com>
2025-01-03 18:05:12 +0800

    修复FIFO长度BUG

--- src/FIFO/FIFO.h
+++ src/FIFO/FIFO.h
@@ -4,174 +4,177 @@ template <typename T, int SIZE>
 class FIFO {
 public:
     T data[SIZE];
-    uint16_t front, rear;
+    uint16_t front;
+    uint32_t len;
     bool overwriteOnFull;
     FIFO():front(0),rear(0), overwriteOnFull(false) {}
     void setOverwriteOnFull(bool value) {
       overwriteOnFull = value;
     }
-    bool enqueue(T &item) {
-      if(length() == SIZE){
-        if (overwriteOnFull) {
-          front = (front + 1) % SIZE; // Overwrite the oldest item
-        } else {
-          return false;  // Overflow
+    bool enqueue(T& item) {
+        if (isFull()) {
+            if (!overwriteOnFull) return false; // Overflow
+            dequeue();
         }
-      }
-      data[rear] = item;
-      rear = (rear + 1) % SIZE;
-      return true;
+        data[getRear()] = item;
+        ++len;
+        return true;
     }
-    bool enqueue(T *item) {
-      if(item == 0) return false; //Not intend to use nullptr
-      if(length() == SIZE){
-        if (overwriteOnFull) {
-          front = (front + 1) % SIZE; // Overwrite the oldest item
-        } else {
-          return false;  // Overflow
+    bool enqueue(T* item) {
+        if (item == nullptr) return false;
+        if (isFull()) {
+            if (!overwriteOnFull) return false; // Overflow
+            dequeue();
         }
-      }
-      data[rear] = *item;
-      rear = (rear + 1) % SIZE;
-      return true;
+        data[getRear()] = *item;
+        ++len;
+        return true;
     }
-    bool dequeue(T **item) {
-      if (isEmpty()) return false;
-      *item = &(data[front]);
-      front = (front + 1) % SIZE;
-      return true;
+    bool dequeue(T** item) {
+        if (isEmpty()) return false;
+        *item = &(data[front]);
+        front = (front + 1) % SIZE;
+        --len;
+        return true;
     }
-    bool dequeue(T *&item){
-      if (isEmpty()) return false;
-      item = &(data[front]);
-      front = (front + 1) % SIZE;
-      return true;
+    bool dequeue(T*& item) {
+        if (isEmpty()) return false;
+        item = &(data[front]);
+        front = (front + 1) % SIZE;
+        --len;
+        return true;
     }
-    bool dequeue(T &item) {
-      if (isEmpty()) return false;
-      item = data[front];
-      front = (front + 1) % SIZE;
-      return true;
+    bool dequeue(T& item) {
+        if (isEmpty()) return false;
+        item = data[front];
+        front = (front + 1) % SIZE;
+        --len;
+        return true;
     }
     bool dequeue() {
-      if (isEmpty()) return false;
-      front = (front + 1) % SIZE;
-      return true;
+        if (isEmpty()) return false;
+        front = (front + 1) % SIZE;
+        --len;
+        return true;
     }
-    bool peek(T &item, uint32_t index){ //类似于dequeue，但是不会改变front指针
-      if (isEmpty()) return false;
-      if(index >= length()) return false;
-      item = data[(front+index)%SIZE];
-      return true;
+    bool peek(T& item, uint32_t index) const { // 类似于dequeue，但是不会改变front指针
+        if (isEmpty() || index >= len) return false;
+        item = data[(front + index) % SIZE];
+        return true;
     }
-    bool peek(T *&item, uint32_t index){ //类似于dequeue，但是不会改变front指针
-      if (isEmpty()) return false;
-      if (index >= length()) return false;
-      item = &(data[(front+index)%SIZE]);
-      return true;
+    bool peek(T*& item, uint32_t index) const { // 类似于dequeue，但是不会改变front指针
+        if (isEmpty() || index >= len) return false;
+        item = &(data[(front + index) % SIZE]);
+        return true;
     }
-    bool peek(T **&item, uint32_t index){ //类似于dequeue，但是不会改变front指针
-      if (isEmpty()) return false;
-      if (index >= length()) return false;
-      *item = &(data[(front+index)%SIZE]);
-      return true;
+    bool peek(T**& item, uint32_t index) const { // 类似于dequeue，但是不会改变front指针
+        if (isEmpty() || index >= len) return false;
+        *item = &(data[(front + index) % SIZE]);
+        return true;
     }
-    inline int length() { return rear>=front?rear-front:SIZE-front+rear; }
-    inline bool isFull(){ return length() == SIZE; }
-    inline bool isEmpty(){ return length() == 0; }
-    inline int emptyLength(){ return SIZE - length(); }
+    inline uint32_t length() const { return len; }
+    inline bool isFull() const { return len == SIZE; }
+    inline bool isEmpty() const { return len == 0; }
+    inline int emptyLength() const { return SIZE - len; }
 };
+
+
 
 template <typename T>
 class DynamicFIFO {
-public:
-    T *data;
-    uint16_t front, rear;
+private:
+    T* data;
+    uint32_t front;
+    uint32_t len;
     uint32_t SIZE;
     bool overwriteOnFull;
-    DynamicFIFO(uint32_t size):front(0),rear(0),overwriteOnFull(false) { SIZE = size; data = new T[SIZE]; }
-    void setOverwriteOnFull(bool value) {
-      overwriteOnFull = value;
+    inline uint32_t getRear() const { return (front + len) % SIZE; }
+public:
+    DynamicFIFO(uint32_t size) : front(0), len(0), overwriteOnFull(false) {
+        SIZE = size;
+        data = new T[SIZE];
     }
     ~DynamicFIFO() { delete[] data; }
-    void resize(uint32_t size) {
-      if(size == SIZE) return;
-      T *newData = new T[size];
-      uint32_t len = length();
-      for(uint32_t i = 0; i < len; i++) {
-        newData[i] = data[(front+i)%SIZE];
-      }
-      delete[] data;
-      data = newData;
+    void setOverwriteOnFull(bool value) {
+        overwriteOnFull = value;
     }
-    bool enqueue(T &item) {
-      if(length() == SIZE){
-        if (overwriteOnFull) {
-          front = (front + 1) % SIZE; // Overwrite the oldest item
-        } else {
-          return false;  // Overflow
+    void resize(uint32_t newSize) {
+        if (newSize == SIZE) return;
+        T* newData = new T[newSize];
+        for (uint32_t i = 0; i < len; ++i) {
+            newData[i] = data[(front + i) % SIZE];
         }
-      }
-      data[rear] = item;
-      rear = (rear + 1) % SIZE;
-      return true;
-    }
-    bool enqueue(T *item) {
-      if(item == 0) return false; //Not intend to use nullptr
-      if(length() == SIZE){
-        if (overwriteOnFull) {
-          front = (front + 1) % SIZE; // Overwrite the oldest item
-        } else {
-          return false;  // Overflow
+        delete[] data;
+        data = newData;
+        SIZE = newSize;
+        front = 0; // Reset front after resizing
+        if (len > newSize) {
+            len = newSize;
         }
-      }
-      data[rear] = *item;
-      rear = (rear + 1) % SIZE;
-      return true;
     }
-    bool dequeue(T **item) {
-      if (isEmpty()) return false;
-      *item = &(data[front]);
-      front = (front + 1) % SIZE;
-      return true;
+    bool enqueue(T& item) {
+        if (isFull()) {
+            if (!overwriteOnFull) return false; // Overflow
+            dequeue();
+        }
+        data[getRear()] = item;
+        ++len;
+        return true;
     }
-    bool dequeue(T *&item){
-      if (isEmpty()) return false;
-      item = &(data[front]);
-      front = (front + 1) % SIZE;
-      return true;
+    bool enqueue(T* item) {
+        if (item == nullptr) return false;
+        if (isFull()) {
+            if (!overwriteOnFull) return false; // Overflow
+            dequeue();
+        }
+        data[getRear()] = *item;
+        ++len;
+        return true;
     }
-    bool dequeue(T &item) {
-      if (isEmpty()) return false;
-      item = data[front];
-      front = (front + 1) % SIZE;
-      return true;
+    bool dequeue(T** item) {
+        if (isEmpty()) return false;
+        *item = &(data[front]);
+        front = (front + 1) % SIZE;
+        --len;
+        return true;
+    }
+    bool dequeue(T*& item) {
+        if (isEmpty()) return false;
+        item = &(data[front]);
+        front = (front + 1) % SIZE;
+        --len;
+        return true;
+    }
+    bool dequeue(T& item) {
+        if (isEmpty()) return false;
+        item = data[front];
+        front = (front + 1) % SIZE;
+        --len;
+        return true;
     }
     bool dequeue() {
-      if (isEmpty()) return false;
-      front = (front + 1) % SIZE;
-      return true;
+        if (isEmpty()) return false;
+        front = (front + 1) % SIZE;
+        --len;
+        return true;
     }
-    bool peek(T &item, uint32_t index){ //类似于dequeue，但是不会改变front指针
-      if (isEmpty()) return false;
-      if(index >= length()) return false;
-      item = data[(front+index)%SIZE];
-      return true;
+    bool peek(T& item, uint32_t index) const { // 类似于dequeue，但是不会改变front指针
+        if (isEmpty() || index >= len) return false;
+        item = data[(front + index) % SIZE];
+        return true;
     }
-    bool peek(T *&item, uint32_t index){ //类似于dequeue，但是不会改变front指针
-      if (isEmpty()) return false;
-      if (index >= length()) return false;
-      item = &(data[(front+index)%SIZE]);
-      return true;
+    bool peek(T*& item, uint32_t index) const { // 类似于dequeue，但是不会改变front指针
+        if (isEmpty() || index >= len) return false;
+        item = &(data[(front + index) % SIZE]);
+        return true;
     }
-    bool peek(T **&item, uint32_t index){ //类似于dequeue，但是不会改变front指针
-      if (isEmpty()) return false;
-      if (index >= length()) return false;
-      *item = &(data[(front+index)%SIZE]);
-      return true;
+    bool peek(T**& item, uint32_t index) const { // 类似于dequeue，但是不会改变front指针
+        if (isEmpty() || index >= len) return false;
+        *item = &(data[(front + index) % SIZE]);
+        return true;
     }
-    inline int length() { return rear>=front?rear-front:SIZE-front+rear; }
-    inline bool isFull(){ return length() == SIZE; }
-    inline bool isEmpty(){ return length() == 0; }
-    inline int emptyLength(){ return SIZE - length(); }
+    inline uint32_t length() const { return len; }
+    inline bool isFull() const { return len == SIZE; }
+    inline bool isEmpty() const { return len == 0; }
+    inline int emptyLength() const { return SIZE - len; }
 };
